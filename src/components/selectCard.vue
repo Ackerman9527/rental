f9e08c6a79a00c50da3b559d614d38b5569ee92c
Ackerman9527 <1325974549@qq.com>
2021-03-26 15:02:54 +0800

    初稿

---
+++ src/components/selectCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <div class="cardbody" :style="{backgroundColor:color}" >
+      {{ name }}
+    </div>
+    <div class="cardtext">
+      {{ name }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    color: {
+      type: String,
+      default:''
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+};
+</script>
+<style lang='scss' scoped>
+.card {
+  width: 120px;
+  height: 100px;
+
+  .cardbody {
+    width: 120px;
+    height: 60px;
+    // background-color: #fa6f58;
+    font-size: 24px;
+    line-height: 60px;
+    text-align: center;
+    font-weight: 800;
+    color: #fff;
+    border-radius: 8px;
+  }
+  .cardtext {
+    text-align: center;
+    line-height: 30px;
+  }
+}
+</style>
